cb421c0a7e6fb36a36a8e37d07905467118b305d
Lucas Ramos <lucas-ramos8@hotmail.com>
2025-05-28 10:50:04 -0300

    feat: Testing update each hour

--- app.py
+++ app.py
@@ -6,33 +6,17 @@ from src.data.use_cases.get_tickets import GetTickets
 from src.infra.db.repositories.tickets_requests_repository import TicketsRequestsRepository
 
 use_case = GetTickets(TicketsRequestsRepository())
-labels, values = use_case.get()
-colors = ['#EE0000', '#3b7c59']
-for i in range(len(labels)):
-    if labels[i] == "Responder":
-        colors[i] = '#EE0000'
-    else:
-        colors[i] = '#3b7c59'
-
 
 st.set_page_config(
-   page_title="BI Solicitations",
-   page_icon="🧊",
+    page_title="BI Solicitations",
+    page_icon="🧊",
 )
 
+# Ocultar menus, cabeçalhos, rodapés
 st.markdown("""
     <style>
-        /* Remove menu, header e footer padrão */
-        #MainMenu, header, footer {
-            visibility: hidden;
-        }
-
-        /* Ocupa toda a largura e altura */
-        .main {
-            padding-left: 0rem;
-            padding-right: 0rem;
-        }
-
+        #MainMenu, header, footer { visibility: hidden; }
+        .main { padding-left: 0rem; padding-right: 0rem; }
         .block-container {
             padding-left: 10rem;
             padding-right: 10rem;
@@ -41,28 +25,21 @@ st.markdown("""
             margin: 0;
             max-width: 100%;
         }
-
-        body {
-            margin: 0;
-            padding: 0;
-            overflow-x: hidden;
-        }
-
-        /* Título sem margem superior exagerada */
-        h1 {
-            margin-top: 0rem;
-            font-size: 40px;
-        }
+        body { margin: 0; padding: 0; overflow-x: hidden; }
+        h1 { margin-top: 0rem; font-size: 40px; }
     </style>
 """, unsafe_allow_html=True)
 
-# Configuração de sessão para controle de tempo
-if 'last_run' not in st.session_state:
-    st.session_state.last_run = time.time()
+# Controla tempo de atualização
+if "last_update" not in st.session_state:
+    st.session_state.last_update = time.time()
+if "refresh_interval" not in st.session_state:
+    st.session_state.refresh_interval = 3600  # 1 hora
 
-
-# Gráfico de pizza com labels e valores maiores
 st.title("Solicitações")
+
+labels, values = use_case.get()
+colors = ['#3b7c59' if label != "Responder" else '#EE0000' for label in labels]
 
 fig = go.Figure(data=[go.Pie(
     labels=labels,
@@ -74,23 +51,20 @@ fig = go.Figure(data=[go.Pie(
 )])
 
 fig.update_layout(
-    height=700,  # Aumenta a altura do gráfico
+    height=700,
     margin=dict(t=100, b=5, l=20, r=20),
-    legend=dict(
-        font=dict(size=16)  # Tamanho maior da legenda
-    )
+    legend=dict(font=dict(size=16))
 )
 
-st.plotly_chart(fig, use_container_width=True)
+st.plotly_chart(fig, use_container_width=True, key=f"plot_{int(st.session_state.last_update)}")
 
-# Rodapé com branding
 st.markdown("""
     <div style="text-align: right; opacity: 0.6; font-size: 18px;">
         <b>Hubnexxo</b> | Soluções Empresariais
     </div>
 """, unsafe_allow_html=True)
 
-now = time.time()
-if now - st.session_state.last_run > 3600:
-    st.session_state.last_run = now
+# Atualiza após intervalo
+if time.time() - st.session_state.last_update > st.session_state.refresh_interval:
+    st.session_state.last_update = time.time()
     st.experimental_rerun()
