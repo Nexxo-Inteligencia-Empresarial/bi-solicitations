6b0218820327b38e17a4dd8cd8b9d0f555b801da
Lucas Ramos <lucas-ramos8@hotmail.com>
2025-06-06 14:42:34 -0300

    fix: Removing dot that should not exists

--- modules/nav.py
+++ modules/nav.py
@@ -4,4 +4,4 @@ import streamlit as st
 def Navbar():
     with st.sidebar:
         st.page_link('app.py', label='Geral')
-        st.page_link('pages/departaments.py.', label='Departamentos')
+        st.page_link('pages/departaments.py', label='Departamentos')
