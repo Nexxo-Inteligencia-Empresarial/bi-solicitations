a96e57a72b0b94b537dce39a23db7f09a7ff6327
Victor Tavares <victavares2019@gmail.com>
2025-06-27 12:19:27 -0300

    feat: create table of sla exceeded

---
+++ modules/table.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import streamlit as st
+
+class Table ():
+
+    def __init__(self, sla_exceeded: list[dict]):
+        self._renomed_columns = {
+            "ticket_id": "ID",
+            "departament": "Departamento",
+            "system": "Sistema",
+            "type": "Tipo",
+            "create_date": "Data de Criação",
+            "conclusion_date": "Data de Conclusão",
+            "due_date": "Prazo",
+            "days_to_conclusion": "Dias para Conclusão",
+            "sla_status": "Status do SLA"
+        }
+        self.__render(sla_exceeded)
+
+    def __render(self, sla_exceeded: list[dict]):
+        st.subheader("Solicitações Fora do SLA")
+        if sla_exceeded:
+            df_fora = pd.DataFrame(sla_exceeded).rename(columns=self._renomed_columns)
+            st.dataframe(df_fora)
+        else:
+            st.info("Nenhuma solicitação fora do SLA encontrado.")
--- src/data/use_cases/get_tickets.py
+++ src/data/use_cases/get_tickets.py
@@ -55,52 +55,82 @@ class GetTickets:
         return dict(datas)
     
     def get_tickets_dates(self):
-
         data = self.tickets_requests_repository.get_tickets_dates()
-
         status_totals = {"Dentro do SLA": 0, "Fora do SLA": 0}
+        fora_sla = []
 
-        for conclusion_date, create_date, _ in data:
-            create_date = datetime.fromisoformat(create_date).date()
-            conclusion_date = datetime.fromisoformat(conclusion_date).date()
-
+        for row in data:
+            conclusion_date = datetime.fromisoformat(row.conclusion_date).date()
+            create_date = datetime.fromisoformat(row.create_date).date()
             dias = (conclusion_date - create_date).days
+            
+            ticket_info = {
+                "ticket_id": row.ticket_id,
+                "departament": row.departament,
+                "system": row.system,
+                "type": row.type,
+                "create_date": create_date,
+                "conclusion_date": conclusion_date,
+                "due_date": row.due_date,
+                "days_to_conclusion": dias,
+                "sla_status": "Dentro do SLA" if dias <= 2 else "Fora do SLA"
+            }
 
             if dias <= 2:
                 status_totals["Dentro do SLA"] += 1
             else:
                 status_totals["Fora do SLA"] += 1
+                fora_sla.append(ticket_info)
 
         labels = list(status_totals.keys())
         values = list(status_totals.values())
-        return labels, values
+
+        return labels, values, fora_sla
+
     
     def get_tickets_dates_filters(self, departament_selected: Optional[List[str]] = None, start_date: Optional[date] = None, end_date: Optional[date] = None) -> Tuple[List[str], List[int]]:
-
         data = self.tickets_requests_repository.get_tickets_dates()
         status_totals = {"Dentro do SLA": 0, "Fora do SLA": 0}
+        sla_exceeded = []
 
-        for conclusion_date, create_date, departament in data:
-            departament = self.__classify_departaments(departament.lower())
-            create_date = datetime.fromisoformat(create_date).date()
-            conclusion_date = datetime.fromisoformat(conclusion_date).date()
-
+        for row in data:
+            
+            
+            departament = self.__classify_departaments(row.departament.lower())
             if not self.__filter_departament(departament, departament_selected):
                 continue
-            
+
+            conclusion_date = datetime.fromisoformat(row.conclusion_date).date()
+            create_date = datetime.fromisoformat(row.create_date).date()
+          
             if start_date and end_date:
                 if not self.__filter_date(conclusion_date, start_date, end_date):
                     continue
 
-            dias = (conclusion_date - create_date).days
-            if dias <= 2:
+            days = (conclusion_date - create_date).days
+
+            ticket_info = {
+                "ticket_id": row.ticket_id,
+                "departament": row.departament,
+                "system": row.system,
+                "type": row.type,
+                "create_date": create_date,
+                "conclusion_date": conclusion_date,
+                "due_date": row.due_date,
+                "days_to_conclusion": days,
+                "sla_status": "Dentro do SLA" if days <= 2 else "Fora do SLA"
+            }
+
+            if days <= 2:
                 status_totals["Dentro do SLA"] += 1
             else:
                 status_totals["Fora do SLA"] += 1
+                sla_exceeded.append(ticket_info)
 
         labels = list(status_totals.keys())
         values = list(status_totals.values())
-        return labels, values
+
+        return labels, values, sla_exceeded
 
     def __filter_departament(self, departament: str, selected: Optional[List[str]]) -> bool:
         if selected is None or not selected:
