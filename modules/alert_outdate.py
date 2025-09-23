d76b92281c5d5324314db4c670166f3c44808d66
Victor Tavares <victavares2019@gmail.com>
2025-09-23 17:17:33 -0300

    fix: correct the outdated alert

--- modules/alert_outdate.py
+++ modules/alert_outdate.py
@@ -1,16 +1,15 @@
-from datetime import datetime
 import streamlit as st
+from datetime import datetime
 
-class  AlertOutdate():
-
-    def __init__(self, last_execution_datas: dict):
+class AlertOutdate:
+    def __init__(self, last_execution_datas: list):
         self.__render(last_execution_datas)
 
-    def __render(self, last_execution_datas):
+    def __render(self, last_execution_datas: list):
         now = datetime.now()
         for system, last_execution in last_execution_datas:
             elapsed_time = now - last_execution
             minutes_passed = elapsed_time.total_seconds() / 60
 
             if minutes_passed >= 45:
-                st.warning(f"Coleta do sistema {system} está desatualizada", icon='⚠️')
+                st.warning(f"Coleta do sistema {system} está desatualizada", icon="⚠️")
