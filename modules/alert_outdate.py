9f956e6c1cdef127346249c63394aba6a6c75129
Victor Tavares <victavares2019@gmail.com>
2025-09-23 17:27:49 -0300

    fix: fix alert

--- modules/alert_outdate.py
+++ modules/alert_outdate.py
@@ -1,13 +1,20 @@
-import streamlit as st
 from datetime import datetime
+from zoneinfo import ZoneInfo
+import streamlit as st
 
 class AlertOutdate:
     def __init__(self, last_execution_datas: list):
         self.__render(last_execution_datas)
 
     def __render(self, last_execution_datas: list):
-        now = datetime.now()
+        # Define fuso horário do Brasil
+        now = datetime.now(ZoneInfo("America/Sao_Paulo"))
+
         for system, last_execution in last_execution_datas:
+            # Se last_execution não tiver timezone, é bom ajustar
+            if last_execution.tzinfo is None:
+                last_execution = last_execution.replace(tzinfo=ZoneInfo("America/Sao_Paulo"))
+
             elapsed_time = now - last_execution
             minutes_passed = elapsed_time.total_seconds() / 60
 
