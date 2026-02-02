9a7c365e34a6f26dc7d024e2d1590594137e77ee
Victor Tavares <victavares2019@gmail.com>
2026-02-02 11:52:56 -0300

    feat:: create employees bar chart

--- pages/employee_dashboard.py
+++ pages/employee_dashboard.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import plotly.express as px
 
 from src.data.use_cases.get_employees_tickets import GetEmployeetickets
 from src.infra.db.repositories.employees_tickets_reporitory import EmployeesTicketsRepository
@@ -16,11 +17,16 @@ st.set_page_config(
 
 def main():
     Navbar()
+
     st.title("📋 Solicitações Respondidas por Funcionário")
+    # -----------------------------
+    # FILTOS E MENUS
+    # -----------------------------
+
     with st.container():
         col1, col2, col3 = st.columns([3, 2, 2])
 
-        employees = [e.upper() for e in use_case.get_employees()]
+        employees = pd.Series(use_case.get_employees()).str.upper().tolist()
 
         with col1:
             selected_employees = st.multiselect(
@@ -40,6 +46,18 @@ def main():
                 format="DD/MM/YYYY"
             )
 
+    with st.container():
+        col1, col2, col3 = st.columns([1, 2, 2])
+
+        with col1:
+            SYSTEMS = ["onvio", "acessórias"]
+            system = st.multiselect(
+                "Sistema",
+                SYSTEMS,
+                default=SYSTEMS
+            )
+
+
     start_date_filter = (
         start_date.strftime("%Y-%m-%d") if start_date and end_date else None
     )
@@ -48,14 +66,20 @@ def main():
         end_date.strftime("%Y-%m-%d") if start_date and end_date else None
     )
 
-    result_data = {}
-    if not selected_employees or "TODOS" in selected_employees:
+    # -----------------------------
+    # BUSCA DOS DADOS
+    # -----------------------------
+
+    if not selected_employees:
         result_data = use_case.get_tickets(
             employee=None,
             start_date=start_date_filter,
             end_date=end_date_filter
         )
+
     else:
+        result_data = {}
+
         for employee in selected_employees:
             data = use_case.get_tickets(
                 employee=employee.lower(),
@@ -63,32 +87,58 @@ def main():
                 end_date=end_date_filter
             )
 
-            for emp, info in data.items():
-                if emp not in result_data:
-                    result_data[emp] = info
-                else:
-                    result_data[emp]["solicitações"].extend(info["solicitações"])
-                    result_data[emp]["total"] += info["total"]
+            result_data.update(data)
 
 
-    rows = []
+    # -----------------------------
+    # RESULT_DATA → DATAFRAME
+    # -----------------------------
+    dfs = [
+        pd.DataFrame(
+            (Mappings.ticket_to_dict(ticket) for ticket in info["solicitações"])
+        ).assign(Colaborador=employee.upper())
 
-    for employee, info in result_data.items():
-        for ticket in info["solicitações"]:
-            row = Mappings.ticket_to_dict(ticket)
-            row["Colaborador"] = employee.upper()
-            rows.append(row)
+        for employee, info in result_data.items()
+        if info["solicitações"]
+    ]
 
-    if not rows:
+    if not dfs:
         st.warning("Nenhum resultado encontrado.")
         st.stop()
 
-    df = pd.DataFrame(rows)
+    df = pd.concat(dfs, ignore_index=True)
 
-    st.markdown("### 📊 Resultados")
+    table_df = (
+        df[df["Sistema"].isin(system)]
+    )
+
+    chart_df = (
+        df[df["Sistema"].isin(system)]  # <- filtro
+        .groupby("Colaborador")
+        .size()
+        .reset_index(name="Solicitações")
+        .sort_values(by="Solicitações", ascending=True)
+    )
+
+
+    # -----------------------------
+    ## GRAPHS AND DATAFRAMES
+    # -----------------------------
+
+    fig = px.bar(
+    chart_df,
+    x="Solicitações",
+    y="Colaborador",
+    orientation="h",
+    text="Solicitações",
+    )
+    fig.update_traces(textposition="outside", cliponaxis=False)
+    st.plotly_chart(fig, use_container_width=True)
+    fig.update_yaxes(
+        tickmode="linear"    )
 
     st.dataframe(
-        df,
+        table_df,
         use_container_width=True,
         hide_index=True
     )
